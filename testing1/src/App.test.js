644f9240f97c42ce66f38d2b6b93b9b43bd99c0d
iki <iki.jito@gmail.com>
2022-10-23 07:59:00 +0800

    list inital conditions and expected working function in text

--- testing1/src/App.test.js
+++ testing1/src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("button has correct initial color", () => {});
+test("button has correct initial text", () => {});
+test("button turns blue when clicked", () => {});
