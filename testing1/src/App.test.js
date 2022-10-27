ca76ba357789c5c1de2b042da2f016eafcc2c4f7
iki <iki.jito@gmail.com>
2022-10-27 17:56:11 +0800

    basic look for element test

--- testing1/src/App.test.js
+++ testing1/src/App.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {});
-test("button has correct initial text", () => {});
+test("button has correct initial color and text", () => {
+  // 1. render component
+  render(<App />);
+
+  // 2. identify element
+  const button = screen.getByRole("button", { name: "Change to blue" });
+
+  // 3. assertion
+  expect(button).toHaveStyle({ backgroundColor: "red" });
+});
 test("button turns blue when clicked", () => {});
