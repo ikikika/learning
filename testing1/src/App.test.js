96fd5446230a04c3212957a6b63b83f009dbd3d5
iki <iki.jito@gmail.com>
2022-10-31 23:58:02 +0800

    test behaviour when clicking button

--- testing1/src/App.test.js
+++ testing1/src/App.test.js
@@ -1,16 +1,22 @@
-import { render, screen } from "@testing-library/react";
-import { logRoles } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+// import { logRoles } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color and text", () => {
+test("button has correct initial color and text, and updates when clicked", () => {
+  // const { container } = render(<App />);
+  // logRoles(container);
+
   // 1. render component
-  const { container } = render(<App />);
-  logRoles(container);
+  render(<App />);
 
   // 2. identify element
   const button = screen.getByRole("button", { name: "Change to blue" });
 
   // 3. assertion
   expect(button).toHaveStyle({ backgroundColor: "red" });
+
+  // 4. event and assertions
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: "blue" });
+  expect(button).toHaveTextContent("Change to red");
 });
-test("button turns blue when clicked", () => {});
