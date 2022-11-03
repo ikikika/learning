f929233ec960de1abc9a50e50cca1f24831aa09a
iki <iki.jito@gmail.com>
2022-11-03 23:34:54 +0800

    unit testing functions

--- testing1/src/App.test.js
+++ testing1/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 // import { logRoles } from "@testing-library/react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 test("button has correct initial color and text, and updates when clicked", () => {
   // const { container } = render(<App />);
@@ -80,4 +80,19 @@ test("Clicked disabled button has gray background and reverts to blue", () => {
   // re-enable button
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle("background-color: blue");
+});
+
+// describe statement groups tests
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+
+  test("Works for one inner capital letter", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
