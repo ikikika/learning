8e22d1a075908cc16439bafe74ffd015b273625d
iki <iki.jito@gmail.com>
2022-12-03 12:00:56 +0800

    write component that gets data from server and pass test

--- testing2/sundaes-on-demand/src/pages/entry/Options.jsx
+++ testing2/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,3 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+
 export default function Options({ optionType }) {
-    return <div />
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  
+  // optionType is 'scoops' or 'toppings
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => setError(true));
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
