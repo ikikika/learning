c073887f6cdd5ee29be4e6bd7c46ea35255d0f5a
iki <iki.jito@gmail.com>
2022-12-21 15:11:19 +0800

    write react components to complete the flow

--- testing2/sundaes-on-demand/src/App.js
+++ testing2/sundaes-on-demand/src/App.js
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 
 import OrderEntry from "./pages/entry/OrderEntry";
 
 import { OrderDetailsProvider } from "./context/OrderDetails";
 import OrderSummary from "./pages/summary/OrderSummary";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 
-function App() {
+export default function App() {
+  // orderPhase needs to be 'inProgress', 'review' or 'completed'
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+
+  let Component = OrderEntry; // default to order page
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
-    <Container>
-      <OrderDetailsProvider>
-        {/* Summary page and entry page need provider */}
-        <OrderEntry />
-        <OrderSummary />
-      </OrderDetailsProvider>
-      {/* Confirmation page does not need provider */}
-    </Container>
+    <OrderDetailsProvider>
+      <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
+    </OrderDetailsProvider>
   );
 }
-
-export default App;
