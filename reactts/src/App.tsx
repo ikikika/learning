90a6684b26e0e8c9692119d0c8fcce030a724f2c
iki <iki.jito@gmail.com>
2023-01-14 17:28:56 +0800

    working with props and types for props

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const todos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div className="App">
-      
+      {/* A component that adds todos */}
+      <TodoList items={todos} />
     </div>
   );
 }
