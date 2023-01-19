f603e159eb696a709e2109357233c635fa7f64d8
iki <iki.jito@gmail.com>
2023-01-19 17:17:25 +0800

    props and state, call parent function with parameter from child

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
-import { Todo } from './todo.model';
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
     // using prevState here guarantees that we get all objects in our previous state
-    setTodos(prevTodos => [
+    setTodos((prevTodos) => [
       ...prevTodos,
-      { id: Math.random().toString(), text: text }
+      { id: Math.random().toString(), text: text },
     ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
   };
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
--- reactts/src/components/TodoList.tsx
+++ reactts/src/components/TodoList.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
 interface TodoListProps {
-  items: {id: string, text: string}[];
-};
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
 
-const TodoList: React.FC<TodoListProps> = props => {
+const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
-      {props.items.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+      {props.items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          {/* bind points to the function in the parent compoenent and allow us to pass in parameters without executing it here */}
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );
