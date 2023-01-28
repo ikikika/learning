02aadf5a1f04e82233e235f452ce9c577f89dda0
iki <iki.jito@gmail.com>
2023-01-28 19:33:28 +0800

    explanation for promises

--- promises/index.js
+++ promises/index.js
@@ -1,11 +1,19 @@
+// a promise is a tool for handling any code that is going to execute at some point in the future
+// when you have some amount of code that's going to execute at some amount of time, the future you want to be thinking promises.
+// to create a promise, create a function with return promise that takes a resolve and reject
+// promise has 3 states, unresolved, resolved, rejected
+// it is up to hte developer to define which state the promise is in
+
 const arg = process.argv[2];
 
 function startGame(inputNumber) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (inputNumber > 5) {
+        // function ends here if developer wants to indicate successful execution
         resolve();
       } else {
+        // function ends here if developer wants to indicate error execution
         reject();
       }
     }, 2000);
@@ -14,9 +22,11 @@ function startGame(inputNumber) {
 
 startGame(arg)
   .then(() => {
+    // once promise is in resolved state, this callback will be called
     console.log("More than 5");
   })
   .catch(() => {
+    // once promise is in rejected state, this callback will be called
     console.log("Less than 5");
   });
 
