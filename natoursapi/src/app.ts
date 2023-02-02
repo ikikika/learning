afc839f5f9644c57f602f5fef52923f7a08a926d
iki <iki.jito@gmail.com>
2023-02-02 23:50:13 +0800

    set up mongoose and connect to mongodb

--- natoursapi/src/app.ts
+++ natoursapi/src/app.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 
+import mongoose from "mongoose";
+
 dotenv.config();
 
 const app = express();
+
+const DB = process.env.DATABASE!.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD!
+);
+
+mongoose.connect(DB, {}).then((con) => {
+  console.log(con.connections);
+  console.log("DB connection successful");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server running on port ${process.env.PORT}`);
