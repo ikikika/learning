b433a2eb7893703683f323c23f5e66ce045eef98
iki <iki.jito@gmail.com>
2023-02-12 14:03:33 +0800

    add in more api features function and extract to separate file

--- natoursapi/src/controllers/tourController.ts
+++ natoursapi/src/controllers/tourController.ts
@@ -1,106 +1,22 @@
-import { Request, RequestHandler } from "express";
-
+import { RequestHandler } from "express";
 import Tour from "../models/tourModel";
-import { Query } from "mongoose";
+import { APIFeatures } from "../utils/apiFeatures";
 
 export const aliasTopTours: RequestHandler = (req, res, next) => {
   req.query.limit = "5";
-  req.query.sort = "-ratingAverage,price";
-  req.query.fields = "name,price,ratingAverage,summary,difficulty";
+  req.query.sort = "price,-ratingsAverage";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
 
-export const createTour: RequestHandler = async (req, res, next) => {
-  try {
-    const newTour = await Tour.create(req.body);
-
-    res.status(201).json({
-      status: "success",
-      data: { tour: newTour },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-class APIFeatures {
-  query: Query<any, any>;
-  queryString: Record<string, any>;
-  constructor(query: Query<any, any>, queryString: object) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-
-    excludedFields.forEach((el) => queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // rplace: // let query = Tour.find(JSON.parse(queryStr));
-    this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-}
-
 export const getAllTours: RequestHandler = async (req, res, next) => {
   try {
-    /*
-    // BUILD QUERY
-    // 1A. BASIC FILTERING
-    // req.query will get parameters from the url, eg ...&sort=1&difficulty=2
-    // mongoose has built-in filtering function
-    // destructuring, create a new object with all the key value pairs in req.query
-    // basic filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    // // remove those fields from queryObj
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B. ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2.SORTING
-    if (req.query.sort && typeof req.query.sort === "string") {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query.sort("-createdAt");
-    }
-
-    // 3. Field Limiting
-    if (req.query.fields && typeof req.query.fields === "string") {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // 4. Pagination
-    const page = req.query.page ? Number(req.query.page) * 1 : 1;
-    const limit = req.query.limit ? Number(req.query.limit) * 1 : 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const noTours = await Tour.countDocuments();
-      if (skip >= noTours) {
-        throw new Error("This page does not exist");
-      }
-    }
-    */
     // EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query).filter();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
     const tours = await features.query;
 
     // SEND RESPONSE
@@ -110,6 +26,22 @@ export const getAllTours: RequestHandler = async (req, res, next) => {
       data: {
         tours,
       },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+export const createTour: RequestHandler = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: { tour: newTour },
     });
   } catch (err) {
     res.status(400).json({
