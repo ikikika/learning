09dbdc932b9d3f6b3a78a99a371f47ac2d3654a8
iki <iki.jito@gmail.com>
2023-02-25 23:51:23 +0800

    handle invalid token error

--- natoursapi/src/controllers/authController.ts
+++ natoursapi/src/controllers/authController.ts
@@ -102,5 +102,21 @@ export const protect = catchAsync(async (req, res, next) => {
   // // 3) Check if user still exists
   const currentUser = await User.findById(decoded!.id);
 
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
+  }
+  // // 4) Check if user changed password after the token was issued
+  // if (currentUser.changedPasswordAfter(decoded.iat)) {
+  //   return next(
+  //     new AppError("User recently changed password! Please log in again.", 401)
+  //   );
+  // }
+  // // GRANT ACCESS TO PROTECTED ROUTE
+  // req.user = currentUser;
   next();
 });
--- natoursapi/src/controllers/errorController.ts
+++ natoursapi/src/controllers/errorController.ts
@@ -24,6 +24,9 @@ const handleValidationErrorDB = (err: any) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again!", 401);
+
 const sendErrorDev = (err: any, res: any) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -67,6 +70,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
 
     sendErrorProd(error, res);
   }
