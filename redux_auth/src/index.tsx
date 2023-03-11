a74d44813a6d9bdc8c22d1da3ac59eb74106b114
iki <iki.jito@gmail.com>
2023-03-12 03:48:37 +0800

    Set up auth slice and link redux to index

--- redux_auth/src/index.tsx
+++ redux_auth/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +10,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+      </Provider>
   </React.StrictMode>
 );
 
