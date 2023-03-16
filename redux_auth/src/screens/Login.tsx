162af0712e6e0265935883dd521e2323969dc202
iki <iki.jito@gmail.com>
2023-03-16 23:59:24 +0800

    hook up redux to login component

--- redux_auth/src/screens/Login.tsx
+++ redux_auth/src/screens/Login.tsx
@@ -1,19 +1,51 @@
-import { useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { useAppDispatch } from "../app/hooks";
+import { setCredentials } from "../features/auth/authSlice";
+import { useLoginMutation } from "../app/services/authService";
+
 const Login = () => {
+  const userRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLDivElement>(null);
   const [user, setUser] = useState("");
   const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const navigate = useNavigate();
 
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    userRef.current!.focus();
+  }, []);
+
   useEffect(() => {
     setErrMsg("");
   }, [user, pwd]);
 
-  const handleSubmit = () => {
-    console.log(user, pwd);
-    navigate("/welcome");
+  const handleSubmit = async () => {
+    try {
+      const userData = await login({ user, pwd }).unwrap();
+      dispatch(setCredentials({ ...userData, user }));
+      setUser("");
+      setPwd("");
+      navigate("/welcome");
+    } catch (err: any) {
+      if (err.originalStatus) {
+        if (err.originalStatus) {
+          // isLoading: true until timeout occurs
+          setErrMsg("No Server Response");
+        } else if (err.originalStatus === 400) {
+          setErrMsg("Missing Username or Password");
+        } else if (err.originalStatus === 401) {
+          setErrMsg("Unauthorized");
+        } else {
+          setErrMsg("Login Failed");
+        }
+        errRef.current!.focus();
+      }
+    }
   };
 
   const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -21,9 +53,15 @@ const Login = () => {
   const handlePwdInput = (e: React.ChangeEvent<HTMLInputElement>) =>
     setPwd(e.target.value);
 
-  const content = (
+  const content = isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <section className="login">
-      <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
         {errMsg}
       </p>
 
@@ -36,6 +74,7 @@ const Login = () => {
           id="username"
           value={user}
           onChange={handleUserInput}
+          ref={userRef}
         />
 
         <label htmlFor="password">Password:</label>
