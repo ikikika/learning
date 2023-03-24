77a9c857db471b0c1cb1437da5b5eb90f4e27178
iki <iki.jito@gmail.com>
2023-03-25 06:52:27 +0800

    removing useState, replacing useState vars with state vars from useReducer

--- reducer/README.md
+++ reducer/README.md
@@ -5,3 +5,29 @@
 - very similiar techniques between useReducer and redux
 - useful when there are several different closely related pieces of state
 - useful when future state values depend on current state
+
+## Implementing
+
+1. comment out all state, getters and setters
+1. import useReducer
+1. destructure state and dispatch
+1. define reducer, state variable and initial state
+
+```
+const [  state     ,      dispatch        ] = useReducer(reducer, { count: initialCount, valueToAdd: 0, });
+          |                  |                                                          |
+      state variable   function to change state                                     initial value
+          |                  |                                                          |
+         \|/                \|/                                                        \|/
+
+const[  count       ,    setCount         ] = useState(                             initialCount         );
+const[  valutToAdd  ,    setValueToAdd    ] = useState(                                 0                );
+```
+
+### useState
+
+- each piece of state defined as a separate variable
+
+### useReducer
+
+- all state for whoile component defined in a single state object
--- reducer/src/App.tsx
+++ reducer/src/App.tsx
@@ -1,32 +1,45 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useReducer } from "react";
 import "./App.css";
+
+interface StateType {
+  count: number;
+  valueToAdd: string;
+}
+
+const reducer = (state: StateType, action: any) => {};
 
 function App() {
   // these 2 pieces of state are very closely related, as they are used together in handleSubmit
   // in increment/decrement, we update state by changing the current value. this is an example of future value of state depending on current value
-  const [count, setCount] = useState(0);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(0);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  console.log(state);
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValueToAdd(+event.target.value);
+    // setValueToAdd(+event.target.value);
   };
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <div className="App">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
@@ -34,7 +47,11 @@ function App() {
 
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
-        <input type="number" value={valueToAdd || ""} onChange={handleChange} />
+        <input
+          type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+        />
         <button>Add it!</button>
       </form>
     </div>
