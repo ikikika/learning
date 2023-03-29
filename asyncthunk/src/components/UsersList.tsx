f34a5aac1d3f068d8802796db67221bdbe354b66
iki <iki.jito@gmail.com>
2023-03-29 20:16:21 +0800

    using loading state, delay loading during dev, skeleton loader

--- asyncthunk/src/components/UsersList.tsx
+++ asyncthunk/src/components/UsersList.tsx
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
   const dispatch = useAppDispatch();
+
+  const { isLoading, data, error } = useAppSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
     // step 6: dispatch the thunk
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <div>users list</div>;
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
--- asyncthunk/src/store/thunks/fetchUsers.ts
+++ asyncthunk/src/store/thunks/fetchUsers.ts
@@ -7,7 +7,17 @@ const fetchUsers = createAsyncThunk("users/fetch", async () => {
   // step 3: make the request
   const response = await axios.get("http://localhost:3005/users");
 
+  // DEV ONLY!!!
+  await pause(1000);
+
   return response.data;
 });
+
+// DEV ONLY!!!
+const pause = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
