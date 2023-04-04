44414896f790dc1ff900391f3ea1b7e39fec6527
iki <iki.jito@gmail.com>
2023-04-04 23:40:25 +0800

    rendering list

--- asyncthunk/src/components/AlbumsList.tsx
+++ asyncthunk/src/components/AlbumsList.tsx
@@ -1,12 +1,40 @@
 import { useFetchAlbumsQuery } from "../store";
 import { UserType } from "../types";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
+interface AlbumType {
+  id: number;
+  title: string;
+}
 
 function AlbumsList({ id, name }: UserType) {
   // step 9: use hook
   const { data, error, isLoading } = useFetchAlbumsQuery({ id, name });
 
-  console.log(data, error, isLoading);
-  return <div>Albums for {name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} className="h-10 w-full" />;
+  } else if (error) {
+    content = <div>Error loading data</div>;
+  } else {
+    content = data.map((album: AlbumType) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
