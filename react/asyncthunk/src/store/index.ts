2da628c8e1800063756ca1956d2841b68109ab87
iki <iki.jito@gmail.com>
2023-04-09 03:24:42 +0800

    rtkq photos step 1-8

--- react/asyncthunk/src/store/index.ts
+++ react/asyncthunk/src/store/index.ts
@@ -2,16 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     // step 7-1: connect to store. [albumsApi.reducerPath] simpley returns the string 'albums'.
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   // step 7-2: middleware
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 
@@ -27,4 +31,13 @@ export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
 
 // step 8: export from store/index.ts
-export { useFetchAlbumsQuery, useAddAlbumMutation } from "./apis/albumsApi";
+export {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "./apis/albumsApi";
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from "./apis/photosApi";
