9bcf02d99d048319eec7c6b6a54089f8888e3dd3
iki <iki.jito@gmail.com>
2023-04-11 19:05:28 +0800

    event, ui, icon components

---
+++ nextjs/routing/src/components/ui/button.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import classes from "./button.module.css";
+import { ReactNode } from "react";
+
+interface ButtonType {
+  link: string;
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+function Button(props: ButtonType) {
+  if (props.link) {
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
