32d7ad6656bf098c3f5a711020a90a962b14dbd6
iki <iki.jito@gmail.com>
2023-04-13 07:34:23 +0800

    using getStaticProps to get data and render page on server

--- nextjs/prerender-data-fetching/src/pages/index.tsx
+++ nextjs/prerender-data-fetching/src/pages/index.tsx
@@ -1,11 +1,35 @@
-function HomePage() {
+// none of this code runs on client side. all runs on dev server
+
+// props returned from getStaticProps
+function HomePage(props: { products: ProductType[] }) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
 
+// if we have this function in our component, this function will be executed first
+export async function getStaticProps() {
+  // always need to return props. this function prepares props for the component
+  return {
+    props: {
+      products: [
+        { id: "p1", title: "Product 1" },
+        { id: "p2", title: "Product 2" },
+      ],
+    },
+    revalidate: 10,
+  };
+}
+
 export default HomePage;
+
+interface ProductType {
+  id: string;
+  title: string;
+}
