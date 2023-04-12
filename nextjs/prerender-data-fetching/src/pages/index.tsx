7516008cfc7cb548fb2563390a4b8ec168c1e327
iki <iki.jito@gmail.com>
2023-04-13 07:42:31 +0800

    using filesystem

--- nextjs/prerender-data-fetching/src/pages/index.tsx
+++ nextjs/prerender-data-fetching/src/pages/index.tsx
@@ -1,4 +1,6 @@
 // none of this code runs on client side. all runs on dev server
+import path from "path"; // core nodejs module. no need install
+import fs from "fs/promises"; // core nodejs module. no need install
 
 // props returned from getStaticProps
 function HomePage(props: { products: ProductType[] }) {
@@ -15,13 +17,27 @@ function HomePage(props: { products: ProductType[] }) {
 
 // if we have this function in our component, this function will be executed first
 export async function getStaticProps() {
+  console.log("(Re-)Generating...");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // process is globally availabel on nodejs
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   // always need to return props. this function prepares props for the component
   return {
     props: {
-      products: [
-        { id: "p1", title: "Product 1" },
-        { id: "p2", title: "Product 2" },
-      ],
+      products: data.products,
     },
     revalidate: 10,
   };
