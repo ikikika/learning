5e7719e34cccafd7fd182b24daa8c87cbb43133c
iki <iki.jito@gmail.com>
2023-04-14 00:02:53 +0800

    implement getStaticProps on all events page

--- nextjs/prerender-data-fetching-project/src/pages/events/index.tsx
+++ nextjs/prerender-data-fetching-project/src/pages/events/index.tsx
@@ -1,11 +1,12 @@
 import EventList from "@/components/events/event-list";
-import { getAllEvents } from "../../../dummy-data";
 import EventsSearch from "@/components/events/events-search";
+import { getAllEvents } from "@/helpers/api-util";
+import { EventType } from "@/types/event.type";
 import { useRouter } from "next/router";
 
-function AllEventsPage() {
+function AllEventsPage(props: { events: EventType[] }) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year: number, month: number) {
     const fullPath = `/events/${year}/${month}`;
@@ -18,6 +19,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
