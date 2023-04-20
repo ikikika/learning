79d6b1fa082302ef7882034d19d77caef92d9854
iki <iki.jito@gmail.com>
2023-04-21 02:07:33 +0800

    refactoring cart

--- mfe/ecomm1/container/src/bootstrap.js
+++ mfe/ecomm1/container/src/bootstrap.js
@@ -1,7 +1,10 @@
 // import 'products/ProductsIndex'; // step 7
 import { mount as productsMount } from 'products/ProductsIndex';
-import 'cart/CartShow'; // this alias is found in cart directory's webpack.config.js
+// import 'cart/CartShow'; // this alias is found in cart directory's webpack.config.js
+import { mount as cartMount } from 'cart/CartShow';
+
 
 console.log('Container!');
 
 productsMount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'));
