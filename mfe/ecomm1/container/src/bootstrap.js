03c9937a9b5523a48e8e3888d995686d96b3884e
iki <iki.jito@gmail.com>
2023-04-21 02:01:36 +0800

    comsuming remote module is another div id in container

--- mfe/ecomm1/container/src/bootstrap.js
+++ mfe/ecomm1/container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex'; // step 7
+// import 'products/ProductsIndex'; // step 7
+import { mount as productsMount } from 'products/ProductsIndex';
 import 'cart/CartShow'; // this alias is found in cart directory's webpack.config.js
 
 console.log('Container!');
+
+productsMount(document.querySelector('#my-products'));
