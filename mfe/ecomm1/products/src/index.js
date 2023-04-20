b13994e91dd678fa90400d6dd288b44caf51853c
iki <iki.jito@gmail.com>
2023-04-21 01:07:00 +0800

    async loading for shared modules

--- mfe/ecomm1/products/src/index.js
+++ mfe/ecomm1/products/src/index.js
@@ -1,14 +1,1 @@
-// step 2: we want to make this file in our remote available to otehr projects
-
-import faker from 'faker';
-
-let products = '';
-
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
-
-products = `<h1>products</h1>${products}`
-
-document.querySelector('#dev-products').innerHTML = products;
+import('./bootstrap');
