d94e59261ec3195ad0199a5970a569538cb20d91
iki <iki.jito@gmail.com>
2023-04-21 08:15:24 +0800

    generic integration container & marketing

---
+++ mfe/project2/container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+// calling mount function in marketing's remoteEntry.js
+// should not alias to any library specific name eg MarketingReactApp
+import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
