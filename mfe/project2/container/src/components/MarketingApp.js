9da8490a142b7f12e91653468017deedbd5089eb
iki <iki.jito@gmail.com>
2023-04-22 03:53:18 +0800

    send pathname from subapp to container to update url in browser

--- mfe/project2/container/src/components/MarketingApp.js
+++ mfe/project2/container/src/components/MarketingApp.js
@@ -2,14 +2,21 @@
 // should not alias to any library specific name eg MarketingReactApp
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('navigation noticed in marketing');
+      onNavigate: (location) => {
+        const {pathname} = history.location;
+        const nextpathName = location.pathname;
+        if( pathname !== nextpathName ){
+          console.log('navigation noticed in marketing');
+          history.push(nextpathName);
+        }
       }
     });
   });
