dd38abc929af7b23ca7ce8f1fc6877b61c2ae5aa
ikikika <iki.jito@gmail.com>
2023-06-02 20:14:44 +0800

    feat: implement useDeferredValue hook

--- use-deferred-value/src/App.tsx
+++ use-deferred-value/src/App.tsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useDeferredValue, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  // sometimes we want some delay after setting input before we get output
+  // eg, inputting search term to trigger filtering api from BE
+  // not efficient if api is called on every keystroke
   const [input, setInput] = useState("")
-  const [output, setOutput] = useState("");
+  
+  // useDeferredValue hook implements a delay before some information is calculated. 
+  // This works in a very similar way to debouncing and throttling since our deferred value will only be calculated after the important state updates have finished running. 
+  const deferredInput = useDeferredValue(input);
 
   function handleChange(e: { target: { value: string; }; }) {
     setInput(e.target.value)
   }
 
   useEffect(() => {
-    setOutput(input);
-  }, [input]);
+    console.log({input, deferredInput});
+  }, [input, deferredInput]);
 
   return (
     <>
@@ -19,10 +26,9 @@ function App() {
       <br/>
       input: {input} 
       <br/>
-      output: {output}
+      output: {deferredInput}
     </>
   )
 }
-
 
 export default App
