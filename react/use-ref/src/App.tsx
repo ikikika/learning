4aac9d4a75ef5e6425d5aa1273e92bd20b9902c4
ikikika <iki.jito@gmail.com>
2023-06-06 19:54:55 +0800

    using useRef to store previosu state value

--- react/use-ref/src/App.tsx
+++ react/use-ref/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   // useRef is always an ohject with a current property, which will take whatever value we pass into useRef
   // current property is what gets persisted between different renders
 
-  // useRef to reference elements inside html
+  // useRef to reference elements inside html (works exactly like document query selector)
   const inputRef = useRef<HTMLInputElement | null>(null);
   function focus() {
     inputRef.current?.focus();
@@ -20,10 +20,17 @@ function App() {
     renderCount.current = renderCount.current + 1;
   });
 
+  // store previous state value
+  const prevName = useRef('');
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
   return (
     <>
       <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
       <div>Name: {name}</div>
+      <div>prevName: {prevName.current}</div>
 
       {/* every time the component is rerendered, this value persisted and is counting up */}
       <div>Render Count: {renderCount.current}</div>
