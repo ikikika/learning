344b8be83b081f3c5f6d1ed3f4d6d1e17365de43
ikikika <iki.jito@gmail.com>
2023-06-06 19:35:02 +0800

    refs used to persist values between renders but do not cause component to rerender

--- react/use-ref/src/App.tsx
+++ react/use-ref/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 
 function App() {
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+
+  // should not use useState+useEfect to count no of times rendered because it will throw react in infinite loop
+  // as useState trigger component update, which triger useEffect, which trigger useState and so on
+  const renderCount = useRef<number>(1);
+  // useRef is always an ohject with a current property, which will take whatever value we pass into useRef
+  // current property is what gets persisted between different renders
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   return (
     <>
       <input value={name} onChange={e => setName(e.target.value)} />
       <div>Name: {name}</div>
+
+      {/* every time the component is rerendered, this value persisted and is counting up */}
+      <div>Render Count: {renderCount.current}</div>
     </>
   )
 }
