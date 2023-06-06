e54128d727baacf7d535fc35b079cba39b2fa12b
ikikika <iki.jito@gmail.com>
2023-06-06 19:46:02 +0800

    useRef to reference html elements, works like document query selector

--- react/use-ref/src/App.tsx
+++ react/use-ref/src/App.tsx
@@ -10,17 +10,25 @@ function App() {
   // useRef is always an ohject with a current property, which will take whatever value we pass into useRef
   // current property is what gets persisted between different renders
 
+  // useRef to reference elements inside html
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  function focus() {
+    inputRef.current?.focus();
+  }
+
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
 
   return (
     <>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
       <div>Name: {name}</div>
 
       {/* every time the component is rerendered, this value persisted and is counting up */}
       <div>Render Count: {renderCount.current}</div>
+
+      <button onClick={focus}>Focus</button>
     </>
   )
 }
