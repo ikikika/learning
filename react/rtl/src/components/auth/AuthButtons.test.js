834e5c353123719311df89c4954506c7ce76a196
ikikika <iki.jito@gmail.com>
2025-01-07 10:00:18 +0800

    running thru debugger and rearranging tests, we find data leakage between tests suites

--- react/rtl/src/components/auth/AuthButtons.test.js
+++ react/rtl/src/components/auth/AuthButtons.test.js
@@ -12,6 +12,44 @@ async function renderComponent() {
   await screen.findAllByRole('link');
 }
 
+describe('when user is signed in', () => {
+  // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
+  createServer([
+    {
+      path: '/api/user',
+      res: () => {
+        return { user: { id: 3, email: 'asdf@asdf.com' } };
+      },
+    },
+  ]);
+
+  test.only('sign in and sign up are not visible', async () => {
+    debugger;
+    await renderComponent();
+
+    const signInButton = screen.queryByRole('link', {
+      name: /sign in/i,
+    });
+    const signUpButton = screen.queryByRole('link', {
+      name: /sign up/i,
+    });
+
+    expect(signInButton).not.toBeInTheDocument();
+    expect(signUpButton).not.toBeInTheDocument();
+  });
+
+  test('sign out is visible', async () => {
+    await renderComponent();
+
+    const signOutButton = screen.getByRole('link', {
+      name: /sign out/i,
+    });
+
+    expect(signOutButton).toBeInTheDocument();
+    expect(signOutButton).toHaveAttribute('href', '/signout');
+  });
+});
+
 describe('when user is not signed in', () => {
   // createServer() ---> GET '/api/user' ---> { user: null }
   createServer([
@@ -23,7 +61,8 @@ describe('when user is not signed in', () => {
     },
   ]);
 
-  test('sign in and sign up are visible', async () => {
+  test.only('sign in and sign up are visible', async () => {
+    debugger;
     await renderComponent();
 
     const signInButton = screen.getByRole('link', {
@@ -47,42 +86,5 @@ describe('when user is not signed in', () => {
     });
 
     expect(signOutButton).not.toBeInTheDocument();
-  });
-});
-
-describe('when user is signed in', () => {
-  // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
-  createServer([
-    {
-      path: '/api/user',
-      res: () => {
-        return { user: { id: 3, email: 'asdf@asdf.com' } };
-      },
-    },
-  ]);
-
-  test('sign in and sign up are not visible', async () => {
-    await renderComponent();
-
-    const signInButton = screen.queryByRole('link', {
-      name: /sign in/i,
-    });
-    const signUpButton = screen.queryByRole('link', {
-      name: /sign up/i,
-    });
-
-    expect(signInButton).not.toBeInTheDocument();
-    expect(signUpButton).not.toBeInTheDocument();
-  });
-
-  test('sign out is visible', async () => {
-    await renderComponent();
-
-    const signOutButton = screen.getByRole('link', {
-      name: /sign out/i,
-    });
-
-    expect(signOutButton).toBeInTheDocument();
-    expect(signOutButton).toHaveAttribute('href', '/signout');
   });
 });
