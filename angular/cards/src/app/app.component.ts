95792c43ce9193498405cc36e1f0ccefbdbc31e2
ikikika <iki.jito@gmail.com>
2025-01-21 05:39:48 +0800

    pass data into child component and render

--- angular/cards/src/app/app.component.ts
+++ angular/cards/src/app/app.component.ts
@@ -11,5 +11,26 @@ import { CardComponent } from './card/card.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today'
+    }
+  ];
+  
 }
