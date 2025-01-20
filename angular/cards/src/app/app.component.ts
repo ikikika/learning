42ab0bb14cddd9561064a3621b364f80d3f5d0b2
ikikika <iki.jito@gmail.com>
2025-01-21 05:21:34 +0800

    create card component and scope css

--- angular/cards/src/app/app.component.ts
+++ angular/cards/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 
+// in ts, this is known as a decorator
 @Component({
+  // this is the name of this component. when angular sees this name in a html file, it will render an instance of this component
   selector: 'app-root',
   imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
