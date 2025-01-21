2db6d03c8d49fbdef2e9f5dfff7267fe885d68ee
ikikika <iki.jito@gmail.com>
2025-01-22 04:44:47 +0800

    conditional display

--- angular/typing/src/app/app.component.ts
+++ angular/typing/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { faker } from "@faker-js/faker"; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   randomText = faker.lorem.sentence();
+  enteredText = '';
 
   onInput(value: string) {
-    console.log(value);
+    this.enteredText = value;
   }
 }
