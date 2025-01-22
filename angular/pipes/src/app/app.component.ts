00eef320afc3a63987d8f2ac55664d9327a53798
ikikika <iki.jito@gmail.com>
2025-01-23 06:56:06 +0800

    adding an input and using a pipe

--- angular/pipes/src/app/app.component.ts
+++ angular/pipes/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TitleCasePipe, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes';
+
+  fullName: string = '';
+
+  onNameChange(value: string) {
+    this.fullName = value;
+  }
 }
+
+// https://angular.dev/guide/templates/pipes
+// Pipes are functions that allow us to transform data format in our template
