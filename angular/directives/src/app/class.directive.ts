cfdd8ce51a5606376f5bbad3d24430ef815d7f98
ikikika <iki.jito@gmail.com>
2025-01-24 03:46:05 +0800

    communicate property to directive

--- angular/directives/src/app/class.directive.ts
+++ angular/directives/src/app/class.directive.ts
@@ -1,15 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
+  @Input() backgroundColor: string | undefined;
+
   // reference the element we are applying the directive to
   // we can then access this element and start changing some of the underlying properties
   // eg add in different styling rules, modify classes, etc
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'orange';
+    // this.element.nativeElement.style.backgroundColor = 'orange';
+
+    // NEVER DO THIS!!! example only
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 2000);
   }
 
 }
