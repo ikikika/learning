fab31e9bc68c05f2425a1eacc1687404d6127831
ikikika <iki.jito@gmail.com>
2025-01-24 03:35:24 +0800

    ng generate directive directivename and simple usage

--- angular/directives/src/app/app.component.ts
+++ angular/directives/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ClassDirective } from './class.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ClassDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
