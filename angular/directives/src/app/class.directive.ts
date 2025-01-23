fba012c6fa27e7f244b0f00789363bcc4b95c101
ikikika <iki.jito@gmail.com>
2025-01-24 03:40:34 +0800

    access and moodify elements from custom directive

--- angular/directives/src/app/class.directive.ts
+++ angular/directives/src/app/class.directive.ts
@@ -1,12 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor() { 
-    console.log('Class Directive Used!');
+  // reference the element we are applying the directive to
+  // we can then access this element and start changing some of the underlying properties
+  // eg add in different styling rules, modify classes, etc
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.backgroundColor = 'orange';
   }
 
 }
