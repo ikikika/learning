fae6d4d76296a8b76244dda4b1d924e01388db48
iki <iki.jito@gmail.com>
2025-01-26 21:32:47 +0000

    basic routing

--- angular/modulesystem/src/app/app.routes.ts
+++ angular/modulesystem/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'elements',
+    loadChildren: () =>
+      import('./elements/elements.module').then((m) => m.ElementsModule),
+  },
+];
