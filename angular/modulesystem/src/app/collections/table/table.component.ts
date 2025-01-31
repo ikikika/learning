344e622b7b52263d742731a07eac77e534bdd23c
ikikika <iki.jito@gmail.com>
2025-02-01 04:02:26 +0800

    import table component

--- angular/modulesystem/src/app/collections/table/table.component.ts
+++ angular/modulesystem/src/app/collections/table/table.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-table',
-  imports: [],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
+  standalone: false
 })
 export class TableComponent {
 
