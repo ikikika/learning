db07ed28391460d206af70eabfd8b826ec588929
ikikika <iki.jito@gmail.com>
2025-02-09 14:39:28 +0800

    importing basic tabs component

--- angular/modulesystem/src/app/collections/tabs/tabs.component.ts
+++ angular/modulesystem/src/app/collections/tabs/tabs.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
   templateUrl: './tabs.component.html',
-  styleUrl: './tabs.component.css'
+  styleUrl: './tabs.component.css',
+  standalone: false
 })
 export class TabsComponent {
 
