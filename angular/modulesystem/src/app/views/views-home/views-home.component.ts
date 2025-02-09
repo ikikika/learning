2f91e4796518e91fe001024bf68e018a13249a03
ikikika <iki.jito@gmail.com>
2025-02-10 07:04:08 +0800

    complete statistic component

--- angular/modulesystem/src/app/views/views-home/views-home.component.ts
+++ angular/modulesystem/src/app/views/views-home/views-home.component.ts
@@ -8,5 +8,10 @@ import { StatisticsComponent } from "../statistics/statistics.component";
   standalone: false
 })
 export class ViewsHomeComponent {
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' }
+  ];
 
 }
