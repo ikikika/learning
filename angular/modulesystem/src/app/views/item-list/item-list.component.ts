eb5bde57042d4aba5979765f26439a09d7184cd8
ikikika <iki.jito@gmail.com>
2025-02-10 07:27:54 +0800

    import items list and render

--- angular/modulesystem/src/app/views/item-list/item-list.component.ts
+++ angular/modulesystem/src/app/views/item-list/item-list.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
-  imports: [],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.css'
+  styleUrl: './item-list.component.css',
+  standalone: false
 })
 export class ItemListComponent {
 
