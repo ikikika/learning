a2b1eb819d5e86c25aeb25001806b2f11c29999d
ikikika <iki.jito@gmail.com>
2025-02-11 12:15:47 +0800

    lifecycle hook ngOnInit

--- angular/modulesystem/src/app/mods/modal/modal.component.ts
+++ angular/modulesystem/src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class ModalComponent {
+  constructor(private element: ElementRef) {
+    console.log(this.element.nativeElement);
+  }
 
+  ngOnInit() {
+    document.body.appendChild(this.element.nativeElement);
+    // we can get access to data that has been provided from a parent component, but we cannot access that data anywhere inside of our component before ngOnInit gets called.
+  }
+  
 }
--- angular/modulesystem/src/app/views/item-list/item-list.component.ts
+++ angular/modulesystem/src/app/views/item-list/item-list.component.ts
@@ -9,4 +9,13 @@ import { Component, Input } from '@angular/core';
 export class ItemListComponent {
   @Input() items: { image: string; title: string; description: string; }[] = [];
 
+  constructor() {
+    console.log('inside contructor', this.items);
+  }
+
+  ngOnInit() {
+    //if we ever need to get access to some data that is being provided by a parent component, the earliest we can access it is inside of NG on init.
+    console.log('inside ngOnInit', this.items);
+  }
+
 }
