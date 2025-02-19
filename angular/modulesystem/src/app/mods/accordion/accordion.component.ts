ad59e6eb70847635a8da08fb2571d919eb7dbde6
ikikika <iki.jito@gmail.com>
2025-02-20 02:37:51 +0800

    expanding accordion item

--- angular/modulesystem/src/app/mods/accordion/accordion.component.ts
+++ angular/modulesystem/src/app/mods/accordion/accordion.component.ts
@@ -8,4 +8,14 @@ import { Component, Input } from '@angular/core';
 })
 export class AccordionComponent {
   @Input() items: { title: string; content: string }[] | [] = [];
+
+  openedItemIndex = -1;
+
+  onClick(index: number) {
+    if (index === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
+  }
 }
