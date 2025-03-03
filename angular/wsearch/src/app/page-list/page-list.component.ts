72ee9345109d0dd685b5e58fa99ee50547f43e29
ikikika <iki.jito@gmail.com>
2025-03-04 05:08:19 +0800

    rendering api results

--- angular/wsearch/src/app/page-list/page-list.component.ts
+++ angular/wsearch/src/app/page-list/page-list.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+interface Page {
+  title: string;
+  wordcount: number;
+  snippet: string;
+}
 
 @Component({
   selector: 'app-page-list',
   standalone: true,
   templateUrl: './page-list.component.html',
-  styleUrl: './page-list.component.scss'
+  styleUrl: './page-list.component.scss',
+  imports: [CommonModule],
 })
 export class PageListComponent {
-  @Input() pages = [];
+  @Input() pages: Page[] = [];
 
 }
