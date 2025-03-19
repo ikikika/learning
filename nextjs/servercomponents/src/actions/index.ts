0d3b47833a96ae8701d59c13e7dfb58f05cd0ce7
ikikika <iki.jito@gmail.com>
2025-03-20 00:58:40 +0800

    wrap db function in trycatch, redirect must be outside

--- nextjs/servercomponents/src/actions/index.ts
+++ nextjs/servercomponents/src/actions/index.ts
@@ -24,28 +24,44 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check the user's inputs and make sure they're valid
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    // throw new Error("This is an error");
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
-  if (typeof code !== "string" || code.length < 10) {
-    return {
-      message: "Code must be longer",
-    };
-  }
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
 
   // // Redirect the user back to the root route
+  // we do not want to put our redirect statements into a trycatch.
+  // always leave them outside
   redirect("/");
 }
