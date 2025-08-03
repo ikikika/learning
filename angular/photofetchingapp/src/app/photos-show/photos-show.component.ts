d441c26d484b6f373213cbaa37a646f0ccf24dd9
ikikika <iki.jito@gmail.com>
2025-08-03 16:04:26 +0800

    generate basic service and component files

---
+++ angular/photofetchingapp/src/app/photos-show/photos-show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos-show',
+  imports: [],
+  templateUrl: './photos-show.component.html',
+  styleUrl: './photos-show.component.scss'
+})
+export class PhotosShowComponent {
+
+}
