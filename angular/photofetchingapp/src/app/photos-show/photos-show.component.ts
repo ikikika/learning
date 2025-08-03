86de1b30220fa5a4fb8f1829305d34d1946bc554
ikikika <iki.jito@gmail.com>
2025-08-03 17:35:25 +0800

    displaying the image

--- angular/photofetchingapp/src/app/photos-show/photos-show.component.ts
+++ angular/photofetchingapp/src/app/photos-show/photos-show.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotosService } from '../photos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-photos-show',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './photos-show.component.html',
   styleUrl: './photos-show.component.scss'
 })
 export class PhotosShowComponent implements OnInit {
 
+  photoUrl: string = "";
+
   constructor(private photosService: PhotosService) {
-    this.photosService.getPhoto().subscribe(() => {});
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
+    this.photosService.getPhoto().subscribe(response => {
+      this.photoUrl = response.urls.regular;
+    });
   }
 
   ngOnInit() {}
