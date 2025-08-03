e6b9aa5b374a045060db6f73608b508ad3e43294
ikikika <iki.jito@gmail.com>
2025-08-03 17:18:07 +0800

    set up the api

--- angular/photofetchingapp/src/app/photos.service.ts
+++ angular/photofetchingapp/src/app/photos.service.ts
@@ -1,5 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +15,16 @@ import { Injectable } from '@angular/core';
 export class PhotosService {
 
   constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    const accessKey = environment.UNSPLASH_ACCESS_KEY;
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+      headers: {
+        Authorization: `Client-ID ${accessKey}`
+      }
+      }
+    );
+  }
 }
