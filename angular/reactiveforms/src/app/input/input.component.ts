beae24e2552f7d7ee0dbb2f8cc476252885057fb
ikikika <iki.jito@gmail.com>
2025-08-04 04:49:28 +0800

    refactor for styling

--- angular/reactiveforms/src/app/input/input.component.ts
+++ angular/reactiveforms/src/app/input/input.component.ts
@@ -6,9 +6,18 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-input',
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
 })
 export class InputComponent {
   @Input() control: FormControl = new FormControl('');
-  
+  @Input() label: string = '';
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    // Show errors if the control is invalid and has a value (not pristine)
+    if (errors && this.control.value !== '' && this.control.invalid) {
+      return true;
+    }
+    return dirty && touched && !!errors;
+  }
 }
