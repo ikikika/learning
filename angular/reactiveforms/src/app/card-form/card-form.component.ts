6d36332a19183a945bd8c61fb9919a7a69621511
ikikika <iki.jito@gmail.com>
2025-08-03 22:29:39 +0800

    ng g c CardForm

---
+++ angular/reactiveforms/src/app/card-form/card-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-form',
+  imports: [],
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.scss'
+})
+export class CardFormComponent {
+
+}
