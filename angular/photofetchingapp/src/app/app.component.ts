7dde18a67f34910f9ecd576a155942fc5b524834
ikikika <iki.jito@gmail.com>
2025-08-03 17:23:18 +0800

    displaying the component

--- angular/photofetchingapp/src/app/app.component.ts
+++ angular/photofetchingapp/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhotosShowComponent } from "./photos-show/photos-show.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PhotosShowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
