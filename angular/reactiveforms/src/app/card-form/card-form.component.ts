764f72efb5f081110357d52d80d7d509a7e392c8
ikikika <iki.jito@gmail.com>
2025-08-05 03:57:29 +0800

    additional validations

--- angular/reactiveforms/src/app/card-form/card-form.component.ts
+++ angular/reactiveforms/src/app/card-form/card-form.component.ts
@@ -23,9 +23,20 @@ export class CardFormComponent implements OnInit {
       // Validators.maxLength(5),
       // Validators.pattern('/\\s'),
     ]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl(''),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(0[1-9]|1[0-2])/[0-9]{2}$'), // MM/YY format
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
   ngOnInit() {
