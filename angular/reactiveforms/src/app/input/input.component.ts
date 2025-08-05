81a56697b9b32f94f75b155e553dc9151530ba97
ikikika <iki.jito@gmail.com>
2025-08-05 18:01:40 +0800

    input masking using ngx-mask

--- angular/reactiveforms/src/app/input/input.component.ts
+++ angular/reactiveforms/src/app/input/input.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+// import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-input',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule, 
+    // NgxMaskDirective
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
+  // providers: [provideNgxMask()],
 })
 export class InputComponent {
   @Input() control: FormControl = new FormControl('');
