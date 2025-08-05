73335bc3b08c400196406b032cbd08d4ec97fbc0
ikikika <iki.jito@gmail.com>
2025-08-05 16:42:26 +0800

    input masking, manually hijack form control values, insert extra characters and reformat input

--- angular/reactiveforms/src/app/date-form-control.ts
+++ angular/reactiveforms/src/app/date-form-control.ts
@@ -1,2 +1,29 @@
-export class DateFormControl {
+import { FormControl } from '@angular/forms';
+
+export class DateFormControl extends FormControl {
+  override setValue(value: string, options: any): void {
+    //     console.log(value)
+    //     super.setValue(value, options);
+
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2) {
+      super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+      return;
+    }
+    super.setValue(value, { ...options, emitModelToViewChange: true });
+  }
 }
