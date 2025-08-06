0fbe195b170e74ef65c2530e97106035c5d0d6fb
ikikika <iki.jito@gmail.com>
2025-08-07 03:48:35 +0800

    basic signup form

--- angular/emailclient/src/app/auth/signup/signup.component.ts
+++ angular/emailclient/src/app/auth/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirm: new FormControl(''),
+  });
 }
