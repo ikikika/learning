639aadbd6503f957c49d690d986ea3e84e738bd3
ikikika <iki.jito@gmail.com>
2025-08-07 12:09:17 +0800

    connecting custom validators (password match)

--- angular/emailclient/src/app/auth/signup/signup.component.ts
+++ angular/emailclient/src/app/auth/signup/signup.component.ts
@@ -1,35 +1,42 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-signup',
+  standalone: true,
   imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {
-  authForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-    passwordConfirm: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-  });
+export class SignupComponent implements OnInit {
+  authForm = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+    },
+    { validators: MatchPassword.validate }
+  );
+
+  ngOnInit() {}
 }
--- angular/emailclient/src/app/auth/validators/match-password.ts
+++ angular/emailclient/src/app/auth/validators/match-password.ts
@@ -1,2 +1,16 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 export class MatchPassword {
+  static validate: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+
+    if (password && passwordConfirm && password !== passwordConfirm) {
+      return { passwordsDontMatch: true };
+    }
+
+    return null;
+  };
 }
