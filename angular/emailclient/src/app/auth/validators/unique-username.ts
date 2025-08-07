157e70b0d1e5cfea77da3f88f8b25891e1118820
ikikika <iki.jito@gmail.com>
2025-08-07 18:19:58 +0800

    handle errors from async validation

--- angular/emailclient/src/app/auth/validators/unique-username.ts
+++ angular/emailclient/src/app/auth/validators/unique-username.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +13,31 @@ import { Observable, of } from 'rxjs';
 export class UniqueUsername implements AsyncValidator {
   constructor(private http: HttpClient) {}
 
-  validate = (control: AbstractControl): Observable<ValidationErrors | null> => {
+  validate = (
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> => {
     const { value } = control;
 
-    console.log('Validating username:', value);
-    console.log(this.http);
-    return of(null); 
+    // console.log('Validating username:', value);
+    // console.log(this.http);
+    // return of(null);
+
+    return this.http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value,
+      })
+      .pipe(
+        map((value) => {
+          return null; // this api is configured if the username is available, it returns null
+        }),
+        catchError((err) => {
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true }); // of operator creates an observable that emits the error object
+        } else {
+            // eg, if the server is down
+            return of({ noConnection: true });
+          }
+        })
+      );
   };
 }
