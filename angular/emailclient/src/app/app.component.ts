e907948bcc8a014583db8b62beecad0aecd90b1b
ikikika <iki.jito@gmail.com>
2025-08-08 03:12:06 +0800

    header basic implementation

--- angular/emailclient/src/app/app.component.ts
+++ angular/emailclient/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AuthModule],
+  imports: [RouterOutlet, AuthModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
