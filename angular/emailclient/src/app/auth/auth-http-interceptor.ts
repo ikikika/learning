8a1223bb505e5d893bd9f65695aea1633197a0e5
ikikika <iki.jito@gmail.com>
2025-08-08 19:02:17 +0800

    using interceptor to modify response

--- angular/emailclient/src/app/auth/auth-http-interceptor.ts
+++ angular/emailclient/src/app/auth/auth-http-interceptor.ts
@@ -4,8 +4,9 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpEventType,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthHttpInterceptor implements HttpInterceptor {
@@ -19,5 +20,19 @@ export class AuthHttpInterceptor implements HttpInterceptor {
     });
 
     return next.handle(modifiedReq);
+
+    // return next.handle(modifiedReq).pipe(
+    //   tap((event) => {
+    //     // can use other operators to msufy or log the response
+    //     if (event.type === HttpEventType.Response) {
+    //       // Handle the response if needed
+    //       console.log('Response received:', event);
+    //     }
+    //     if (event.type === HttpEventType.Sent) {
+    //       // Handle the request being sent if needed
+    //       console.log('Request sent:', event);
+    //     }
+    //   })
+    // );
   }
 }
