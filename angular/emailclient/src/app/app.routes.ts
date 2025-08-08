1b069c98b00732931d84cd7794b570d0a76c9161
ikikika <iki.jito@gmail.com>
2025-08-09 03:15:41 +0800

    routing to inbox home

--- angular/emailclient/src/app/app.routes.ts
+++ angular/emailclient/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'inbox',
+    loadChildren: () =>
+      import('./inbox/inbox.module').then(mod => mod.InboxModule)
+  }
+];
