d30cdd0c771b65c7068d180547fea41dcee5eaaa
ikikika <iki.jito@gmail.com>
2025-08-09 11:04:27 +0800

    implement guards for protected routes

--- angular/emailclient/src/app/app.routes.ts
+++ angular/emailclient/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-    {
+  {
     path: 'inbox',
     loadChildren: () =>
-      import('./inbox/inbox.module').then(mod => mod.InboxModule)
-  }
+      import('./inbox/inbox.module').then((mod) => mod.InboxModule),
+    canMatch: [authGuard],
+  },
 ];
--- angular/emailclient/src/app/auth/auth.guard.ts
+++ angular/emailclient/src/app/auth/auth.guard.ts
@@ -1,5 +1,21 @@
-import { CanMatchFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, Router, UrlTree } from '@angular/router';
+import { map, skipWhile, take, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
-export const authGuard: CanMatchFn = (route, segments) => {
-  return true;
+export const authGuard: CanMatchFn = (): Observable<boolean | UrlTree> => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.signedin$.pipe(
+    // skipWhile: if your stream emits null, wait until it returns true or false
+    skipWhile((value) => value === null),
+    take(1),
+    tap((authenticated) => {
+      if (!authenticated) {
+        router.navigateByUrl('/');
+      }
+    })
+  );
 };
