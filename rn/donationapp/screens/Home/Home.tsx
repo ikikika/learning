db572c42fb2805f7f944b0ba8fc8344b37e55a24
iki <iki.jito@gmail.com>
2025-09-07 01:11:36 +0800

    create button component using Pressable

--- rn/donationapp/screens/Home/Home.tsx
+++ rn/donationapp/screens/Home/Home.tsx
@@ -3,12 +3,20 @@ import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const Home = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
         <Header title={'Azzahri A.'} type={1} />
+        <Button
+          title={'Donate'}
+          onPress={() => {
+            console.log('You just pressed me!');
+          }}
+        />
+        <Button title={'Donate'} isDisabled={true} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
