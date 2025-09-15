f0451f59c1b18e3bad75b4a1a0500bf221f7e055
iki <iki.jito@gmail.com>
2025-09-15 20:43:55 +0800

    create search input component

---
+++ rn/donationapp/components/Search/Search.tsx
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react';
+import { Pressable, TextInput } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { scaleFontSize } from '../../assets/styles/scaling';
+import style from './style';
+
+const Search = ({ onSearch }: SearchTypes) => {
+  const textInputRef = useRef<TextInput>(null);
+  const [search, setSearch] = useState('');
+
+  const handleFocus = () => {
+    textInputRef.current?.focus();
+  };
+
+  const handleSearch = (searchValue: string) => {
+    setSearch(searchValue);
+    onSearch(searchValue);
+  };
+  return (
+    <Pressable style={style.searchInputContainer} onPress={handleFocus}>
+      <FontAwesomeIcon
+        icon={faSearch}
+        color={'#25C0FF'}
+        size={scaleFontSize(22)}
+      />
+      <TextInput
+        ref={textInputRef}
+        style={style.searchInput}
+        value={search}
+        onChangeText={value => handleSearch(value)}
+      />
+    </Pressable>
+  );
+};
+
+interface SearchTypes {
+  onSearch: (searchValue: string) => void;
+}
+
+export default Search;
--- rn/donationapp/screens/Home/Home.tsx
+++ rn/donationapp/screens/Home/Home.tsx
@@ -2,21 +2,17 @@ import React from 'react';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import globalStyle from '../../assets/styles/globalStyle';
-import Header from '../../components/Header/Header';
-import Tab from '../../components/Tab/Tab';
-import Badge from '../../components/Badge/Badge';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Search from '../../components/Search/Search';
 
 const Home = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-        <Header title={'Azzahri A.'} type={1} />
-        <Tab title={'Highlight'} />
-        <Tab title={'Highlight'} isInactive={true} />
-        <Badge title={'Environment'} />
-        <FontAwesomeIcon icon={faSearch} />
+        <Search
+          onSearch={value => {
+            console.log(value);
+          }}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
