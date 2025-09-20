9010a47ceafae00e05a09a19eb970f77b19d0e2f
iki <iki.jito@gmail.com>
2025-09-20 15:41:54 +0800

    redux logger

--- rn/donationapp/redux/store.ts
+++ rn/donationapp/redux/store.ts
@@ -7,6 +7,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // Importing the User reducer from the ./reducers/User file
 import UserReducer from './reducers/User';
 
+// import { logger } from 'redux-logger';
 // Creating a rootReducer that combines all reducers in the app
 
 const rootReducer = combineReducers({
@@ -18,6 +19,9 @@ const rootReducer = combineReducers({
 // We're passing in the rootReducer as the main reducer for the store
 const store = configureStore({
   reducer: rootReducer,
+  // middleware: getDefaultMiddleware => {
+  //   return getDefaultMiddleware().concat(logger);
+  // },
 });
 
 // Exporting the store to be used in the app
