8a1a5981549f015af3d88e0894614e7f324fee87
iki <iki.jito@gmail.com>
2025-09-22 05:05:42 +0800

    persisting store

--- rn/donationapp/redux/store.ts
+++ rn/donationapp/redux/store.ts
@@ -10,18 +10,39 @@ import UserReducer from './reducers/User';
 // import { logger } from 'redux-logger';
 // Creating a rootReducer that combines all reducers in the app
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
 const rootReducer = combineReducers({
   // Here, we're combining the User reducer and calling it "user"
   user: UserReducer,
 });
 
+// Configuring the redux-persist library to persist the root reducer with AsyncStorage
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+// Creating a new persisted reducer with the configuration and root reducer
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 // Creating a new Redux store using the configureStore function
-// We're passing in the rootReducer as the main reducer for the store
+// We're passing in the persisted reducer as the main reducer for the store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   // middleware: getDefaultMiddleware => {
   //   return getDefaultMiddleware().concat(logger);
   // },
+
+  // Using the getDefaultMiddleware function from the Redux Toolkit to add default middleware to the store
+  // We're passing in an object with the serializableCheck key set to false to avoid serialization errors with non-serializable data
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
 });
 
 // Exporting the store to be used in the app
@@ -29,3 +50,6 @@ export default store;
 
 // RootState type
 export type RootState = ReturnType<typeof store.getState>;
+
+// exporting the persistor object created with the persistStore function from redux-persist
+export const persistor = persistStore(store);
