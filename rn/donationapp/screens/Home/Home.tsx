672022b427d24a77bfbc2dab1362fadf37a68a23
iki <iki.jito@gmail.com>
2025-10-05 04:02:14 +0800

    pagination through categories flatlist (loading categories sequentially)

--- rn/donationapp/screens/Home/Home.tsx
+++ rn/donationapp/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SetStateAction, useEffect, useState } from 'react';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import globalStyle from '../../assets/styles/globalStyle';
@@ -36,6 +36,33 @@ const Home = () => {
 
   const categories = useAppSelector(state => state.categories);
   console.log(categories);
+
+  const [categoryPage, setCategoryPage] = useState(1);
+  const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+  const categoryPageSize = 4;
+
+  useEffect(() => {
+    setIsLoadingCategories(true);
+    setCategoryList(
+      pagination({
+        items: categories.categories,
+        pageNumber: categoryPage,
+        pageSize: categoryPageSize,
+      }),
+    );
+    setCategoryPage(prev => prev + 1);
+    setIsLoadingCategories(false);
+  }, []);
+
+  const pagination = ({ items, pageNumber, pageSize }: PaginationType) => {
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    if (startIndex >= items.length) {
+      return [];
+    }
+    return items.slice(startIndex, endIndex);
+  };
 
   return (
     <SafeAreaProvider>
@@ -77,9 +104,30 @@ const Home = () => {
           </View>
           <View style={style.categories}>
             <FlatList
+              onEndReachedThreshold={0.5}
+              onEndReached={() => {
+                if (isLoadingCategories) {
+                  return;
+                }
+                console.log(
+                  'User has reached the end and we are getting more data for page number ',
+                  categoryPage,
+                );
+                setIsLoadingCategories(true);
+                let newData = pagination({
+                  items: categories.categories,
+                  pageNumber: categoryPage,
+                  pageSize: categoryPageSize,
+                });
+                if (newData.length > 0) {
+                  setCategoryList(prevState => [...prevState, ...newData]);
+                  setCategoryPage(prevState => prevState + 1);
+                }
+                setIsLoadingCategories(false);
+              }}
               horizontal={true}
               showsHorizontalScrollIndicator={false}
-              data={categories.categories}
+              data={categoryList}
               renderItem={({ item }) => (
                 <View style={style.categoryItem} key={item.categoryId}>
                   <Tab
@@ -101,3 +149,14 @@ const Home = () => {
 };
 
 export default Home;
+
+interface CategoryType {
+  categoryId: number;
+  name: string;
+}
+
+interface PaginationType {
+  items: CategoryType[];
+  pageNumber: number;
+  pageSize: number;
+}
